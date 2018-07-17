e107d796302bb4ff142af3bd68b608b63a3c976b
woodardc <woodardc@goldmail.etsu.edu>
2018-07-17 13:49:03 -0400

    Initial Commit.

---
+++ MVC-KnockoutMVC-PostSharp-WebApp/MVC-KnockoutMVC-PostSharp-WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_KnockoutMVC_PostSharp_WebApp.MvcApplication" Language="C#" %>
---
+++ MVC-KnockoutMVC-PostSharp-WebApp/MVC-KnockoutMVC-PostSharp-WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_KnockoutMVC_PostSharp_WebApp.Startup))]
+namespace MVC_KnockoutMVC_PostSharp_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
